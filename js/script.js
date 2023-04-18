f81b3d872bbac3a23834aa554c19b7c68423e3c1
Oh Ga Eun <dhrkdms4113@naver.com>
2023-04-18 21:54:22 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,92 @@
+let allMemo = JSON.parse(localStorage.getItem("allMemo"));
+allMemo = allMemo ?? [];
+
+const Editor = toastui.Editor;
+
+const editor = new Editor({
+  el: document.querySelector("#editor"),
+  height: "600px",
+  initialEditType: "markdown",
+  previewStyle: "vertical",
+});
+
+render(); // render 가 있어야 새로고침 시 저장 내용들이 불러와져있음
+
+function saveNote() {
+  const title = document.getElementById("title").value;
+  const content = editor.getHTML();
+
+  allMemo.push({ title, content, len: allMemo.length });
+
+  localStorage.setItem("allMemo", JSON.stringify(allMemo));
+  render();
+}
+
+function render() {
+  const display = document.getElementById("display");
+  display.innerHTML = "";
+
+  for (const item of allMemo) {
+    const saveTitle = document.createElement("h2");
+    const saveContent = document.createElement("div");
+    const saveId = document.createElement("p");
+    const deleteMemoBtn = document.createElement("button");
+
+    saveTitle.textContent = item.title;
+    saveContent.innerHTML = item.content;
+    saveId.textContent = item.len + 1;
+    deleteMemoBtn.textContent = "삭제";
+    deleteMemoBtn.setAttribute("id", item.len);
+    deleteMemoBtn.setAttribute("onclick", "remove()");
+
+    display.appendChild(saveId);
+    display.appendChild(saveTitle);
+    display.appendChild(saveContent);
+    display.appendChild(deleteMemoBtn);
+  }
+}
+
+function remove() {
+  const idx = allMemo.find((item) => item.len == event.srcElement.id);
+  if (idx) {
+    allMemo.splice(allMemo.findIndex((item) => item.len == idx.len), 1);
+  }
+  localStorage.setItem("allMemo", JSON.stringify(allMemo));
+  render();
+}
+
+// 날짜
+const $year = document.querySelector('.year');
+const $month = document.querySelector('.month');
+const $date = document.querySelector('.date');
+
+let year = new Date().getFullYear();
+let month = new Date().getMonth() + 1;
+let date = new Date().getMonth();
+
+$year.textContent = `${year}년`;
+$month.textContent = `${month}월`;
+$date.textContent = `${date}일`;
+
+
+// 날씨
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "a6b736388f27635d259edc2d80c58d4b"
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url) // js가 url 주소를 불러줌
+    .then((Response) => Response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = data.weather[0].main;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError); // 위치 좌표
