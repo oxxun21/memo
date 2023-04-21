e71c8beeb6a69a66eeca69f33926ca863125c031
Oh Ga Eun <dhrkdms4113@naver.com>
2023-04-21 18:18:51 +0900

    1차 기능, 디자인 수정

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const Editor = toastui.Editor;
 const editor = new Editor({
   el: document.querySelector("#editor"),
   height: "600px",
-  initialEditType: "wysiwyg",
+  initialEditType: "markdown",
   previewStyle: "vertical",
 });
 
@@ -16,37 +16,53 @@ function saveNote() {
   const title = document.getElementById("title").value;
   const content = editor.getHTML();
 
-  allMemo.push({ title, content, len: allMemo.length });
+  if (true) { // 에디터 안에 내용이 비어있으면 등록 X 기능
+    allMemo.push({ title, content, len: allMemo.length });
 
-  localStorage.setItem("allMemo", JSON.stringify(allMemo));
+    localStorage.setItem("allMemo", JSON.stringify(allMemo));
+  }
   render();
 }
 
-function render() {
+function render(){
   const display = document.getElementById("display");
+  const editor = document.querySelector('.ProseMirror').querySelector('div');
+  const title = document.querySelector('#title');
   display.innerHTML = "";
 
-  for (const item of allMemo) {
+  for (const item of allMemo){
+    const wrapDiv = document.createElement("div");
+    const contentDiv = document.createElement("div");
     const saveTitle = document.createElement("h2");
     const saveContent = document.createElement("div");
-    const saveId = document.createElement("p");
+    const saveDate = document.createElement("span");
     const deleteMemoBtn = document.createElement("button");
 
+    let year = new Date().getFullYear();
+    let month = new Date().getMonth() + 1;
+    let date = new Date().getDate();
+
+    contentDiv.classList.add('contentDiv')
     saveTitle.textContent = item.title;
     saveContent.innerHTML = item.content;
-    saveId.textContent = item.len + 1;
+    saveDate.textContent = `${year}년 ${month}월 ${date}일`;
     deleteMemoBtn.textContent = "삭제";
+    deleteMemoBtn.classList.add('btn');
     deleteMemoBtn.setAttribute("id", item.len);
     deleteMemoBtn.setAttribute("onclick", "remove()");
 
-    display.appendChild(saveId);
-    display.appendChild(saveTitle);
-    display.appendChild(saveContent);
-    display.appendChild(deleteMemoBtn);
+    display.appendChild(wrapDiv);
+    wrapDiv.appendChild(contentDiv);
+    contentDiv.appendChild(saveTitle);
+    contentDiv.appendChild(saveDate);
+    contentDiv.appendChild(saveContent);
+    wrapDiv.appendChild(deleteMemoBtn);
   }
+  editor.innerHTML = "";
+  title.value = "";
 }
 
-function remove() {
+function remove(){
   const idx = allMemo.find((item) => item.len == event.srcElement.id);
   if (idx) {
     allMemo.splice(allMemo.findIndex((item) => item.len == idx.len), 1);
@@ -55,7 +71,16 @@ function remove() {
   render();
 }
 
-// 토스트 에디터 초기화 기능
+const clear = document.getElementById('clear');
+clear.addEventListener('click', () => {
+  alert('제목과 본문을 삭제합니다.')
+
+  const editor = document.querySelector('.ProseMirror').querySelector('div');
+  const title = document.querySelector('#title');
+
+  editor.innerHTML = ""
+  title.value = ""
+})
 
 // 날짜
 const $year = document.querySelector('.year');
@@ -64,7 +89,7 @@ const $date = document.querySelector('.date');
 
 let year = new Date().getFullYear();
 let month = new Date().getMonth() + 1;
-let date = new Date().getMonth();
+let date = new Date().getDate();
 
 $year.textContent = `${year}년`;
 $month.textContent = `${month}월`;
